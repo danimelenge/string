51f2b9cc6327bb82e4eea9d517df171580a378a2
daniel melenge <danielmelenge@gmail.com>
2023-04-04 12:28:47 -0500

    Initial Commit

---
+++ DaniM.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+
+// Collections  // Array
+
+var supermarketList: [String] = ["Eggs",    "Milk", "Bread",    "Coffee",   "Sugar"]
+
+var SupermarketList = ["Eggs",  "Milk"]
+
+print("The Supermarket List  contains \(SupermarketList.count)  items")
+
+print("The supermarket List contains    \(supermarketList.count)    items")
+
+supermarketList.append("Cheese")
+
+var firstItem = supermarketList[0]
+var secondItem = supermarketList[1]
+var thirdItem = supermarketList[2]
+
+supermarketList.capacity
+supermarketList.last
+supermarketList.endIndex
+supermarketList.joined()
+supermarketList.enumerated()
+supermarketList.min()
+supermarketList.startIndex
+supermarketList.max()
+supermarketList.sorted()
+supermarketList.sort()
+supermarketList.shuffled()
+supermarketList.removeLast()
+supermarketList.removeFirst()
+
+for item  in    supermarketList{
+    print(item)
+}
+
+for (index, value)  in  supermarketList.enumerated()    {
+    print("Item \(index+1): \(value)")
+}
+
+
+
+
+
+
+
+
+
+
